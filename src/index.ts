6b5ffd72ff143e5a95b4b07d5c7c713c17643ade
sobhi69 <mhegazy6371@gmail.com>
2025-05-08 14:25:02 +0300

    commit 2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 const app = express()
 const port = 3006   
-
+import userRouter from './routes/user'
 app.get('/',(req,res) => {
     res.json({message:"Thanks God"})
 })
+
+
+app.use('/api/user',userRouter)
+
 
 app.listen(port,() => console.log(`app is alive at http://localhost:${port}`))
